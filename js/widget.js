f2928ce889e09e8d938fb62d35f1d5013687a9c1
Brexton <brextonpham@stanford.edu>
2015-07-14 19:05:10 -0400

    Added roomRequest widget

--- js/widget.js
+++ js/widget.js
@@ -18,3 +18,24 @@ xhr.onreadystatechange = function () {
 };
 xhr.open('GET', 'data/employees.json');
 xhr.send();
+
+var roomRequest = new XMLHttpRequest();
+roomRequest.onreadystatechange = function () {
+	if(roomRequest.readyState === 4) {
+		var rooms = JSON.parse(roomRequest.responseText);
+		var statusHTML = '<ul class="rooms">';
+		for (var i=0; i < rooms.length; i++) {
+			if (rooms[i].available === true) {
+				statusHTML += '<li class="empty">';
+			} else {
+				statusHTML += '<li class="full">';
+			}
+			statusHTML += rooms[i].room;
+			statusHTML += '</li>';
+		}
+		statusHTML += '</ul>';
+		document.getElementById('roomList').innerHTML = statusHTML;
+	}
+};
+roomRequest.open('GET', '../data/rooms.json');
+roomRequest.send();
